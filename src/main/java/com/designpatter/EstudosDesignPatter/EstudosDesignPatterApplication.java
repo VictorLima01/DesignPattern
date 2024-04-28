37378c7ed0bbedef336ee0314187b450ff4b03d6
VictorLima01 <victor.lima.araujo@gmail.com>
2024-04-28 10:39:34 -0300

    First commit

---
+++ src/main/java/com/designpatter/EstudosDesignPatter/EstudosDesignPatterApplication.java
@@ -0,0 +1,13 @@
+package com.designpatter.EstudosDesignPatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosDesignPatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosDesignPatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designpatter/EstudosDesignPatter/EstudosDesignPatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designpatter.EstudosDesignPatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosDesignPatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
